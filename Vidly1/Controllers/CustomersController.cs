8002ce4ce4f0dd749790779e0b46b37f38107cb3
2000a <2000a@DESKTOP-NKSL82H>
2021-10-06 13:48:03 +0530

    Add project files.

---
+++ Vidly1/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using Vidly1.Models;
+
+namespace Vidly1.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext context;
+        public CustomersController()
+        {
+            context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = context.Customers.Include(x=>x.membershipType).ToList();
+            return View(customers);
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = context.Customers.Include(x => x.membershipType).SingleOrDefault(x => x.Id == id);
+
+             return View(customer);
+        }
+    }
+}
